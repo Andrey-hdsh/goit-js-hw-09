c6c99fd07c4483b0045dee32820de4445029f135
Andrey-hdsh <hdsh138720@gmail.com>
2023-09-20 14:35:23 +0300

    commit_1.2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 };
 
-intervalColorId = null;
+let intervalColorId = null;
 btnStopColor.disabled = true;
 
 function startColorSwitching() {
